1e9d6a60adccc20e3b670702720132d6d7c02fbc
titusbonke <titusbonkec@gmail.com>
2024-03-30 09:29:35 +0530

    .

--- src/app/Pages/todo-app/Components/todo-entry/todo-entry.component.ts
+++ src/app/Pages/todo-app/Components/todo-entry/todo-entry.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-entry',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-entry.component.html',
   styleUrl: './todo-entry.component.css'
 })
 export class TodoEntryComponent {
-
+  TodoText:string="";
+  AddTodo = ()=>{
+    console.log(this.TodoText)
+  }
 }
