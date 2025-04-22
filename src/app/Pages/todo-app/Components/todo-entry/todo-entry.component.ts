c2e668689a88f48740f83a14b44d5c74430ff639
titusbonke <titusbonkec@gmail.com>
2025-04-22 16:54:27 +0530

    test commit

---
+++ src/app/Pages/todo-app/Components/todo-entry/todo-entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-entry',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-entry.component.html',
+  styleUrl: './todo-entry.component.css'
+})
+export class TodoEntryComponent {
+
+}
